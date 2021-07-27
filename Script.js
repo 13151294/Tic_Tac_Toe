cff358b8a4d76f1df0b2743aa2d77f54bdbf2a96
Ace1294 <83032267+13151294@users.noreply.github.com>
2021-07-27 08:42:15 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -5,10 +5,7 @@ var board = [
 ];
 var AllWinPattern = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6],];
 //1 = player 1, player / 2 = player 2, Bot
-var turn = "2";
-var player1 = 'Player 1';
-var player2 = 'Player 2';
-var Bot = false;
+var turn = "1";
 //swap player
 const turn_dict = new Map();turn_dict.set("1","2");turn_dict.set("2","1");
 win = false;
@@ -34,81 +31,16 @@ function CheckWin(now_turn) {
   } return false;
 }
 
-function evaluate(Board) {
-  if (CheckWin("2")) {
-    return 10;
-  } else if (CheckWin("1")) {
-    return -10;
-  } else {
-    return 0;
-  }
-}
-
-function minimax(Board, depth, max) {
-  let score = evaluate(board);
-  if (score != 0 || BoardFull()) {return score}
-  if (max) {
-    let best = -100;
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        alert();
-        if (Board[i][j] == " ") {
-          Board[i][j] = "2";
-          best = Math.max(best, minimax(Board, depth+1, !max));
-          Board[i][j] = " ";
-        }
-      }
-    } return best
-  } else {
-    let best = 100;
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        alert(Board[i][j]);
-        if (Board[i][j] == " ") {
-          Board[i][j] = "1";
-          best = Math.min(best, minimax(Board, depth+1, !max));
-          Board[i][j] = " ";
-        }
-      }
-    } return best
-  }
-}
-
-function BestMove(Board) {
-  let best_value = -100;
-  let best_move = [-1,-1];
-  
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (Board[i][j] == " ") {
-        Board = "2";
-        let move_value = minimax(Board, 0, false);
-        Board[i][j] = " ";
-        if (move_value > best_move) {
-          best_move = [i, j];
-          best_value = move_value;
-        }
-      }
-    }
-  }Move(best_move[0], best_move[1], "2");
-}
-
 function Reset() {
   location.reload();
 }
 
 function Move(row, col, turn) {
-  cell = document.getElementById("cell_"+(3*row+col));
-  if (turn == "1") {
-    cell.style.background = "linear-gradient(326deg, #a4508b 0%, #5f0a87 74%)";
-  } else if (turn == "2") {
-    cell.style.background = "linear-gradient(315deg, #7ee8fa 0%, #80ff72 74%)";
-  }
   board[row][col] = turn;
   win = CheckWin(turn);
   filled = BoardFull();
   if (win) {
-    turn = (turn = '1') ? player1:player2;
+    turn = (turn == '1') ? "Player 1":"Player 2";
     document.getElementById("State").innerHTML = turn + " Win!!";
     for (let i = 0; i < 9; i++) {
       document.getElementById("cell_"+i).disabled = true;
@@ -122,32 +54,15 @@ function Move(row, col, turn) {
 }
 
 function Click(row, col) {
-  document.getElementById("Mode").disabled = true;  
+  cell = document.getElementById("cell_"+(3*row+col));
   if (board[row][col] == " ") {
+    if (turn == "1") {
+      cell.style.background = "linear-gradient(326deg, #a4508b 0%, #5f0a87 74%)";
+    } else if (turn == "2") {
+      cell.style.background = "linear-gradient(315deg, #7ee8fa 0%, #80ff72 74%)";
+    }
     Move(row, col, turn);
-  }
-  if (Bot) {
-    BestMove(board);
-  } else {
+    cell.disabled = true;
     turn = turn_dict.get(turn);
-  }
-}
-function ChangeMode(Mode) {
-  if (Mode == 'AI') {
-    document.getElementById("Mode").setAttribute("onclick", "javascript: ChangeMode('Player');")
-    document.getElementById("Mode").innerHTML = 'AI';
-    document.getElementById("P1").innerHTML = 'Player';
-    document.getElementById("P2").innerHTML = 'AI';
-    Bot = true;
-    player1 = 'Player';
-    player2 = 'Agent A';
-  } else if (Mode == 'Player') {
-    document.getElementById("Mode").setAttribute("onclick", "javascript: ChangeMode('AI');")
-    document.getElementById("Mode").innerHTML = 'Player';
-    document.getElementById("P1").innerHTML = 'Player 1';
-    document.getElementById("P2").innerHTML = 'Player 2';
-    Bot = false;
-    player1 = 'Player 1';
-    player2 = 'Player 2';
   }
 }
